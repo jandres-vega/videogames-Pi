4ac7e507739166a7e22f579c3b8eed3f2244abc7
Andres vega <jandresvega@itsa.edu.co>
2022-10-20 19:35:12 -0500

    Agregando webpack al proyecto client

---
+++ client/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('__dirname', 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve : {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'css-loader',
+                    'style-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png)$/,
+                type: 'asset'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        compress: true,
+        historyApiFallback: true,
+        open: true,
+        port: 3006
+    }
+}
